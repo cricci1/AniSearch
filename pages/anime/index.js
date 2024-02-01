5a107a0f11f198d4c5a85639f22ab1bee7446004
Christian Ricci <cricci3@myseneca.ca>
2024-02-01 12:31:26 -0500

    Version 0.1.1

--- pages/anime/index.js
+++ pages/anime/index.js
@@ -1,18 +1,16 @@
-import { Container, Row } from 'react-bootstrap'
+import { Container } from 'react-bootstrap'
 import Image from 'next/image'
 import Link from 'next/link'
-import CharacterCard from '@/components/CharacterCard';
 
 export default function Anime() {
 
     return (
         <>
         <Container className="justify-content-center text-center">
-            <Row>
-            <Col>
-            <CharacterCard></CharacterCard>
-            </Col>
-            </Row>
+            <Image src="/wip-gif.webp" width="500" height="300"></Image>
+            <br/><br/>
+            <h1>This page is still being worked on!</h1>
+            <h4>While your here, why don&apos;t you check <Link href="/anime/1">this</Link> out?</h4>
         </Container>
         </>
     );
